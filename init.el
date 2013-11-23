258dc4edc43747ec01589f5c05f4a5757fe9e04f
Jens Jensen <jens@jeggsen.com>
2013-11-23 16:56:41 +0100

    added a bunch of handy functions

--- init.el
+++ init.el
@@ -141,5 +141,11 @@
 (require 'smex)
 (smex-initialize)
 
+;; Functions (load all files in defuns-dir)
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
+
 ;; Load custom keybindings
 (require 'key-bindings)
