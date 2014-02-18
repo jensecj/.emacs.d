34c6cd81ad98772b2cbe02198db45b99380d6496
Jens Jensen <jens@jeggsen.com>
2014-02-18 11:31:39 +0100

    set default fill column width to 80

--- init.el
+++ init.el
@@ -134,6 +134,7 @@
 (require 'fill-column-indicator)
 (setq fci-rule-width 1)
 (setq fci-rule-color "dark gray")
+(setq fci-rule-column 80)
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
 (global-fci-mode 1)
 
