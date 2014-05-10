b1375f910290cfd70c0bbce13a8ab0eb000cecd9
Jens Jensen <jeggsen@gmail.com>
2014-05-10 12:32:34 +0200

    added warning to toggle-window-split

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -40,7 +40,8 @@
           (set-window-buffer (selected-window) this-win-buffer)
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
+          (if this-win-2nd (other-window 1))))
+    (message "You can't toggle splits of a single window!")))
 
 (defun rotate-windows ()
   "Rotate your windows"
