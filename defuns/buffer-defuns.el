1f0628a46ded83605972e5d6e3cfb37ad3322778
Jens Jensen <jeggsen@gmail.com>
2014-05-20 16:18:29 +0200

    moved window functions to window-defuns.el

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -68,50 +68,9 @@
              (set-window-start w2 s1)
              (setq i (1+ i)))))))
 
-;; intuitive window resizing
-(defun xor (b1 b2)
-  (or (and b1 b2)
-      (and (not b1) (not b2))))
-
-(defun move-border-left-or-right (arg dir)
-  "General function covering move-border-left and move-border-right. If DIR is
-     t, then move left, otherwise move right."
-  (interactive)
-  (if (null arg) (setq arg 3))
-  (let ((left-edge (nth 0 (window-edges))))
-    (if (xor (= left-edge 0) dir)
-        (shrink-window arg t)
-      (enlarge-window arg t))))
-
-(defun move-border-up-or-down (arg dir)
-  "General function covering move-border-up and move-border-down. If DIR is
-     t, then move up, otherwise move down."
-  (interactive)
-  (if (null arg) (setq arg 3))
-  (let ((top-edge (nth 1 (window-edges))))
-    (if (xor (= top-edge 0) dir)
-        (shrink-window arg nil)
-      (enlarge-window arg nil))))
-
-(defun move-border-left (arg)
-  (interactive "P")
-  (move-border-left-or-right arg t))
-
-(defun move-border-right (arg)
-  (interactive "P")
-  (move-border-left-or-right arg nil))
-
-(defun move-border-up (arg)
-  (interactive "P")
-  (move-border-up-or-down arg t))
-
-(defun move-border-down (arg)
-  (interactive "P")
-  (move-border-up-or-down arg nil))
-
 (defun cleanup-buffer ()
   "Perform a bunch of operations on the white space content of a buffer.
-Including indent-buffer, which should not be called automatically on save."
+   Including indent-buffer, which should not be called automatically on save."
   (interactive)
   (whitespace-cleanup)
   (indent-region (point-min) (point-max))
