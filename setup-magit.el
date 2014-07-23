7e362aaebf39fb58218db963f6ae096dee7bed28
Jens Jensen <jeggsen@gmail.com>
2014-07-23 08:37:37 +0200

    show amend commit message

--- setup-magit.el
+++ setup-magit.el
@@ -4,16 +4,6 @@
   ad-do-it
   (delete-other-windows))
 
-;; 'C-c C-a' to amend without any prompt
-(defun magit-just-amend ()
-  (interactive)
-  (save-window-excursion
-    (magit-with-refresh
-     (shell-command "git --no-pager commit --amend --reuse-message=HEAD"))))
-
-(eval-after-load "magit"
-  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
-
 ;; 'q' to quit magit session
 (defun magit-quit-session ()
   "Restores the previous window configuration and kills the magit buffer"
