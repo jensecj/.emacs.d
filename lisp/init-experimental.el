c312db17cdc08ba82eb009c027aa9c0b06d3c475
Jens Jensen <jeggsen@gmail.com>
2015-02-12 14:49:41 +0100

    testing some el-get and coq

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -1,3 +1,10 @@
-this error is here on purpose
+
+;; testing some el-get
+(require 'el-get)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
+(el-get 'sync)
+
+;; install proofgeneral for coq
+(el-get-bundle ProofGeneral)
 
 (provide 'init-experimental)
