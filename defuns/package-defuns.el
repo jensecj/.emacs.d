3043ad578b341c438b29dee0ca8aead73615ac94
Jens <jensecj@gmail.com>
2017-11-17 12:26:47 +0100

    added package-defuns.el, extra functions for some packages

---
+++ defuns/package-defuns.el
@@ -0,0 +1,34 @@
+;; a place for functions i wish were included in their respective packages
+
+(defun counsel-read-file-name (prompt &optional initial-input)
+  "Find a file path using ivy-read"
+  (interactive)
+  (ivy-read prompt 'read-file-name-internal
+            :matcher #'counsel--find-file-matcher
+            :initial-input initial-input
+            :action
+            (lambda (x)
+              (with-ivy-window
+                (if (and counsel-find-file-speedup-remote
+                         (file-remote-p ivy--directory))
+                    (let ((find-file-hook nil))
+                      (expand-file-name x ivy--directory))
+                  (expand-file-name x ivy--directory))))
+            :preselect (when counsel-find-file-at-point
+                         (require 'ffap)
+                         (let ((f (ffap-guesser)))
+                           (when f (expand-file-name f))))
+            :require-match 'confirm-after-completion
+            :history 'file-name-history
+            :keymap counsel-find-file-map
+            :caller 'counsel-read-find-name))
+
+(defun ivy-dir-done ()
+  "Exit the minibuffer with current directory instead of current candidate."
+  (interactive)
+  (delete-minibuffer-contents)
+  (insert (setf (ivy-state-current ivy-last)
+                ivy--directory))
+  (setq ivy-completion-beg ivy-completion-end)
+  (setq ivy-exit 'done)
+  (exit-minibuffer))
