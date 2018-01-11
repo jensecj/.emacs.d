b4a06db3a65085599a0cf450dab5d22a3b4d3397
Jens <jensecj@gmail.com>
2018-01-11 15:47:14 +0100

    config.org swallowed init-keybindings.el, init.el is now a one-liner

--- init.el
+++ init.el
@@ -1,20 +1,7 @@
-;; load default packages
+;; load and activate default packages
 (package-initialize)
 
 (require 'org)
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-(defun try-require (feature)
-  "Tries to require FEATURE, if an exception is thrown, log it."
-  (condition-case ex
-      (progn
-        (message (format "@ \e[94m Loading \"%s\" \e[0m" (symbol-name feature)))
-        (require feature))
-    ('error (message (format "@ \e[1m\e[31m Error loading \"%s\": %s \e[0m" (symbol-name feature) ex)))))
-
-;; Safely load all the init files
-(message "# \e[96m Started loading init files \e[0m")
-(try-require 'init-keybindings)
-(message "# \e[96m Finished loading init files \e[0m")
 
 (provide 'init)
