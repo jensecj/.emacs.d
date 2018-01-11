2574b51dbf355b9061f2a52bca45ae8d339f37dc
Jens <jensecj@gmail.com>
2018-01-11 18:56:27 +0100

    init.el now has all the required bootstrapping code to fresh-install

--- init.el
+++ init.el
@@ -1,7 +1,28 @@
 ;; load and activate default packages
 (package-initialize)
 
-(require 'org)
+;; setup package archives
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")))
+
+;; install use-package if we don't already have it
+(unless (package-installed-p 'use-package)
+  (message (format "= \e[1m\e[31muse-package is not installed! installing...\e[0m"))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; I want to use the new version of org-mode from upstream:
+
+;; remove the built-in org-mode from the load path, so it does not get loaded
+(setq load-path (remove-if (lambda (x) (string-match-p "org$" x)) load-path))
+;; remove org-mode from the built-ins list, because we're using upstream
+(setq package--builtins (assq-delete-all 'org package--builtins))
+
+;; install upstream org-mode
+(use-package org :ensure t :pin org)
+
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
 (provide 'init)
