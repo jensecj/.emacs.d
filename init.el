36a5e049ac9591000f2f8060a1c055c88d923010
Jens Christian Jensen <jensecj@gmail.com>
2018-04-18 13:29:04 +0200

    disable some things that slow down booting emacs

--- init.el
+++ init.el
@@ -6,6 +6,14 @@
 
 (message (format "= \e[1m\e[32mStarted initializing emacs!\e[0m"))
 
+;; disable some things that slow down startup
+;; there's no need to be stirngent about garbage collection when starting up
+(setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
+;; also we dont need to figure out the file-handlers for the files we visit,
+;; we're only visiting config files
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 ;; setup package archives
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
@@ -33,6 +41,10 @@
 (use-package org :ensure org-plus-contrib :pin org)
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;; reset the things we disables earlier
+(setq gc-cons-threshold 16777216 gc-cons-percentage 0.1)
+(setq file-name-handler-alist default-file-name-handler-alist)
 
 (message (format "= \e[1m\e[32mEmacs initialized in %s\e[0m" (emacs-init-time)))
 
