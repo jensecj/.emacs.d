9dbc23ac51c0a6ced4ffbe992dcf57ceae063c42
Jens Christian Jensen <jensecj@gmail.com>
2018-05-07 21:42:20 +0200

    disable automatic quickhelp with auto-complete

--- init.el
+++ init.el
@@ -1035,7 +1035,7 @@ restores the message."
   (setq ac-show-menu t) ;; show the menu instantly
   (setq ac-show-menu-immediately-on-auto-complete t) ;; show the autocompletion menu instantly
   (setq ac-delay 0.1) ;; show completion menu quickly
-  (setq ac-use-quick-help t) ;; use the help
+  (setq ac-use-quick-help nil) ;; use the help
   (setq ac-quick-help-delay 0.1) ;; show help quickly
   (setq ac-use-comphist t)
   (setq ac-comphist-file (concat my-emacs-data-dir "ac-history")) ;; move the history file
