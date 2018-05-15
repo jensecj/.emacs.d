06eece4df520e2cf50603775c78711a25395682d
Jens Christian Jensen <jensecj@gmail.com>
2018-05-15 13:14:04 +0200

    minor changes to today.el. added today-list, which lists all plans

--- lisp/today.el
+++ lisp/today.el
@@ -1,10 +1,11 @@
+;;; -*- lexical-binding: t -*-
 ;;; today.el --- simple daily planning
 
 ;; Copyright (C) 2018 Jens Christian Jensen
 
 ;; Author: Jens Christian Jensen <jensecj@gmail.com>
 ;; Keywords: fullscreen
-;; Package-Version: 20180514
+;; Package-Version: 20180515
 ;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -29,6 +30,8 @@
 ;; move the subtree-at-point to the planning file for that date.
 ;; `today-go-to' will prompt for a date using the org-calendar, then jump to the
 ;; corresponding planning file for that date.
+;; `today-list' will list the dates for all planning files in `today-directory',
+;; selecting one will jump to the corresponding file.
 
 ;; you can add all the planning files to `org-agenda-files' by adding the
 ;; `today-directory', and then changing `org-agenda-file-regex' to match the
@@ -40,51 +43,55 @@
 ;;; Code:
 
 (require 'f)
+(require 'dash)
 
 (defcustom today-directory
   (concat user-emacs-directory "planning")
   "Directory used for planning files.")
 
-(defun today--current-date ()
-  "Date string for the current date."
-  (format-time-string "%Y-%m-%d"))
-
 (defun today--file-from-date (date)
   "Planning file corresponding to DATE."
   (f-join today-directory date (concat date ".org")))
 
-(defun today--create-file (path)
+(defun today--create-path (path)
   "Creates a planning file with PATH."
+  ;; create the planning directory if it does not exist
+  (unless (f-exists? today-directory)
+    (f-mkdir today-directory))
+
   (let ((dir (f-dirname path)))
+    ;; create directory of PATH if it does not exist
     (unless (f-exists? dir)
       (f-mkdir dir)))
 
+  ;; finally, create the file of PATH
   (unless (f-exists? path)
     (f-touch path)))
+
+(defun today--find-file-by-date (date)
+  "Jump to the planning file for DATE, create it if it does not exist."
+  (letrec ((file (today--file-from-date date)))
+    (today--create-path file)
+    (find-file file)))
 
 (defun today ()
   "Open todays planning file, create it if it does not exist."
   (interactive)
-  ;; create the planning directory if it does not exist
-  (unless (f-directory? today-directory)
-    (f-mkdir today-directory))
+  (letrec ((todays-date (format-time-string "%Y-%m-%d")))
+    (today--find-file-by-date todays-date)))
 
-  ;; create todays directory if it does not exist
-  (unless (f-directory? today-directory)
-    (f-mkdir today-directory))
-
-  (letrec ((todays-date (today--current-date))
-           (todays-file (today--file-from-date todays-date)))
-    (today--create-file todays-file)
-    (find-file todays-file)))
+(defun today-list ()
+  "List all dates from `today-directory', jump to the one selected."
+  (interactive)
+  (letrec ((dates (-map #'f-base (f-directories today-directory)))
+           (date (completing-read "Date: " dates)))
+    (today--find-file-by-date date)))
 
 (defun today-go-to ()
   "Prompt for date, and go to corresponding planning file."
   (interactive)
-  (letrec ((date (org-read-date))
-           (file (today--file-from-date date)))
-    (today--create-file file)
-    (find-file file)))
+  (letrec ((date (org-read-date)))
+    (today--find-file-by-date date)))
 
 (defun today-move-to-tomorrow ()
   "Move a subtree from a planning file to tomorrows planning file."
@@ -94,22 +101,23 @@
            (tomorrows-date-in-seconds (+ todays-date-in-seconds seconds-in-a-day))
            (tomorrows-date (format-time-string "%Y-%m-%d" tomorrows-date-in-seconds))
            (tomorrows-file (today--file-from-date tomorrows-date)))
-    (today--create-file tomorrows-file)
+    (today--create-path tomorrows-file)
 
     (org-cut-subtree)
 
-    (let ((tomorrows-buffer (find-file tomorrows-file)))
-      (with-current-buffer tomorrows-buffer
-        (org-mode)
-        (goto-char (point-max))
-        (yank)))))
+    (save-excursion
+      (let ((tomorrows-buffer (find-file-other-window tomorrows-file)))
+        (with-current-buffer tomorrows-buffer
+          (org-mode)
+          (goto-char (point-max))
+          (yank))))))
 
 (defun today-move-to-date ()
   "Moves a subtree to the selected date."
   (interactive)
   (letrec ((date (org-read-date))
            (new-file (today--file-from-date date)))
-    (today--create-file new-file)
+    (today--create-path new-file)
 
     (org-cut-subtree)
 
