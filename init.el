0eb3aebfce025947f91fd6c9895daa8c7ffc2b2b
Jens Christian Jensen <jensecj@gmail.com>
2018-05-28 20:25:17 +0200

    added function to download link to current working dir.

--- init.el
+++ init.el
@@ -2308,6 +2308,15 @@ Use `ivy-pop-view' to delete any item from `ivy-views'."
           (delete-region (region-beginning) (region-end))
           (insert org-link)))))
 
+(defun download-to-current-buffer-directory (link)
+  "Download LINK into the working directory for the current buffer."
+  (interactive)
+  (letrec ((-silence-output " -so - ")
+           (-use-user-agent "'-A User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36'")
+           (filename (f-filename link))
+           (curl-command (concat "curl" " " -use-user-agent " '" link "'" -silence-output " > " filename)))
+    (s-trim (shell-command-to-string curl-command))))
+
 (use-package evil
   :ensure t
   :disabled t)
