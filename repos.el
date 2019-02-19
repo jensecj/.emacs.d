86735e8110ca67af3d28af7ab8ab68c6a83b2456
Jens Christian Jensen <jensecj@gmail.com>
2019-02-19 23:03:06 +0100

    added views.el to repos.el

--- repos.el
+++ repos.el
@@ -1,11 +1,14 @@
 (
  ("~/.dotfiles/")
+
  ("~/.emacs.d/init.el")
  ("~/.emacs.d/straight/repos/doc-at-point.el/")
  ("~/.emacs.d/straight/repos/etmux.el/")
  ("~/.emacs.d/straight/repos/lowkey-mode-line.el/")
  ("~/.emacs.d/straight/repos/replace-at-point.el/")
  ("~/.emacs.d/straight/repos/sane-windows.el/")
+ ("~/.emacs.d/straight/repos/views.el/")
+
  ("~/vault/blog/")
  ("~/vault/git/")
  ("~/vault/knowledge-base/")
