bcd1e0e8dd9fa145dc6e85af972b52c2729e44c7
Jens Christian Jensen <jensecj@gmail.com>
2019-02-20 12:55:16 +0100

    updated repos.el

--- repos.el
+++ repos.el
@@ -9,11 +9,12 @@
  ("~/.emacs.d/straight/repos/sane-windows.el/")
  ("~/.emacs.d/straight/repos/views.el/")
 
- ("~/vault/blog/")
  ("~/vault/git/")
  ("~/vault/knowledge-base/")
  ("~/vault/projects/clojure/microblog/")
  ("~/vault/projects/code-challenges/")
  ("~/vault/projects/java/flatlab/")
  ("~/vault/projects/python/jackup/")
+ ("~/vault/projects/rust/jepa/")
+ ("~/vault/blog/")
  )
