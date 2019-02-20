35151136bb80b420d20f312ede6e623a97527b89
Jens Christian Jensen <jensecj@gmail.com>
2019-02-20 12:54:48 +0100

    switched from `ivy-views` to `views.el`

--- init.el
+++ init.el
@@ -2249,10 +2249,7 @@ paste for multi-term mode."
              ivy-default-view-name
              ivy--get-window)
   :bind
-  (("M-p p" . ivy-push-view)
-   ("M-p k" . ivy-pop-view)
-   ("M-p b" . ivy-switch-view)
-   ("C-x C-b" . ivy-switch-buffer)
+  (("C-x C-b" . ivy-switch-buffer)
    :map ivy-minibuffer-map
    ("C-d" . (lambda () (interactive) (ivy-quit-and-run (dired ivy--directory))))
    ("C-S-<return>" . ivy-immediate-done))
@@ -2260,80 +2257,9 @@ paste for multi-term mode."
   (setq ivy-height 15)
   (setq ivy-count-format "")
   (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-
   (setq ivy-re-builders-alist '((t . ivy--regex-plus)))
 
-  ;; Adds functionality to persist ivy-views across sessions.
-  ;; You could simple add =ivy-views= to =savehist-additional-variables=, but I
-  ;; decided to do it this way, so it saves the view straight to disk when added, and
-  ;; I was modifying =ivy-push-view= anyway, to handle overwriting a view, and
-  ;; changing the default name for views.
-  (defvar ivy-save-file (concat my-emacs-data-dir "ivy-views")
-    "The file on disk used to save ivy-views")
-
-  (defun jens/ivy-save-views ()
-    "Save ivy-views to disk"
-    (interactive)
-    (jens/save-to-file ivy-views ivy-save-file))
-
-  (defun jens/ivy-load-views ()
-    "Load ivy-views from disk"
-    (interactive)
-    (setq ivy-views (jens/load-from-file ivy-save-file)))
-
-  ;; use an empty string as the default view name, instead of buffers
-  (defun jens/ivy-empty-default-view-name ()
-    "Default name for a new view, used in push-view prompt."
-    '"{} ")
-
-  (defun jens/ivy-views-find (view)
-    "Find a view from its name"
-    (dolist (v ivy-views)
-      (if (string= view (car v))
-          (return v))))
-
-  (defun jens/ivy-push-view ()
-    "Push the current window tree on `ivy-views'.
-Currently, the split configuration (i.e. horizonal or vertical)
-and point positions are saved, but the split positions aren't.
-Use `ivy-pop-view' to delete any item from `ivy-views'."
-    (interactive)
-    (let* ((view (cl-labels
-                     ((ft (tr)
-                          (if (consp tr)
-                              (if (eq (car tr) t)
-                                  (cons 'vert
-                                        (mapcar #'ft (cddr tr)))
-                                (cons 'horz
-                                      (mapcar #'ft (cddr tr))))
-                            (with-current-buffer (window-buffer tr)
-                              (cond ((buffer-file-name)
-                                     (list 'file (buffer-file-name) (point)))
-                                    ((eq major-mode 'dired-mode)
-                                     (list 'file default-directory (point)))
-                                    (t
-                                     (list 'buffer (buffer-name) (point))))))))
-                   (ft (car (window-tree)))))
-           (view-name (ivy-read "Name view: " ivy-views
-                                :initial-input (ivy-default-view-name))))
-      (when view-name
-        ;; pop the view if it already exists, so we replace it
-        (ivy-pop-view-action (jens/ivy-views-find view-name))
-        (push (list view-name view) ivy-views))))
-
-  (advice-add 'ivy-push-view :override #'jens/ivy-push-view)
-
-  ;; replace the default view-name
-  (advice-add 'ivy-default-view-name :override #'jens/ivy-empty-default-view-name)
-  ;; (advice-remove 'ivy-default-view-name #'jens/ivy-empty-default-view-name)
-
-  ;; save ivy-views when pushing/popping views
-  (advice-add 'ivy-push-view :after #'jens/ivy-save-views)
-  (advice-add 'ivy-pop-view :after #'jens/ivy-save-views)
-
   (ivy-mode)
-  (jens/ivy-load-views)
   :custom-face
   (ivy-current-match ((t (:foreground nil :background "#4f4f4f" :box t)))))
 
@@ -2464,6 +2390,13 @@ initial search query."
 (use-package blog
   :functions (blog-publish)
   :load-path "~/vault/blog/src/")
+
+(use-package views
+  :straight (views :type git :repo "git@github.com:jensecj/views.el.git")
+  :bind
+  (("M-p p" . views-push)
+   ("M-p k" . views-pop)
+   ("M-p b" . views-switch)))
 
 (use-package sane-windows
   :straight (sane-windows :type git :repo "git@github.com:jensecj/sane-windows.el.git")
