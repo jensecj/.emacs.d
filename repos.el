cfa1528c5ef998e3adf09fa3d72656cd78b2047a
Jens Christian Jensen <jensecj@gmail.com>
2019-06-11 14:18:01 +0200

    updated repos.el

--- repos.el
+++ repos.el
@@ -3,7 +3,7 @@
 
  ("~/.emacs.d/init.el")
 
- ("~/.emacs.d/straight/repos/doc-at-point.el/")
+ ("~/.emacs.d/straight/repos/dokumat.el/")
  ("~/.emacs.d/straight/repos/etmux.el/")
  ("~/.emacs.d/straight/repos/lowkey-mode-line.el/")
  ("~/.emacs.d/straight/repos/replace-at-point.el/")
