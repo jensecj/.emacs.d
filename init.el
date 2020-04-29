26537798ce3d6092bcafa6ff11d9e73afef51556
Jens Christian Jensen <jensecj@posteo.net>
2020-04-29 18:49:17 +0200

    call #'emacs-version interactively at startup-logging to fix spacing

--- init.el
+++ init.el
@@ -11,7 +11,8 @@
 (defun log-success (txt) (message "@ %s" txt))
 
 (log-info "Started initializing emacs!")
-(message "Version: %s" (call-interactively #'emacs-version))
+
+(call-interactively #'emacs-version)
 (message "Commit: %s (%s)" emacs-repository-version emacs-repository-branch)
 
 (log-info "Doing early initialization")
