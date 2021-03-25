d6f972f5d8cbcd1c38cd0b57b2b09fce6f5203bd
Jens C. Jensen <jens@subst.net>
2021-03-25 17:26:30 +0100

    update preference for splitting windows horizontally in ediff

--- init.el
+++ init.el
@@ -911,7 +911,7 @@ seconds."
   :defer t
   :config
   (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-  (setq ediff-split-window-function 'split-window-vertically))
+  (setq ediff-split-window-function 'split-window-horizontally))
 
 (use-package diff
   :config
