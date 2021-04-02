5764ca139e94b2b092f9d6b1c55cbfe434397067
Jens C. Jensen <jens@subst.net>
2021-04-02 13:38:18 +0200

    dired: sorting is handled by args to dired-ls

--- init.el
+++ init.el
@@ -876,16 +876,6 @@ seconds."
   (setq dired-recursive-deletes 'always)
   (setq dired-recursive-copies 'always)
 
-  (defun dired/sort ()
-    "Sort dired listings with directories first."
-    (save-excursion
-      (let (buffer-read-only)
-        (forward-line 2) ;; beyond dir. header
-        (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
-      (set-buffer-modified-p nil)))
-
-  (advice-add #'dired-readin :after #'dired/sort)
-
   ;; use bigger fringes in dired-mode
   (add-hook 'dired-mode-hook (lambda () (setq left-fringe-width 10)))
 
